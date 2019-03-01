db9bd77dd9956bce93fac1db553601798d2a86b3
Your Name <you@example.com>
2019-03-01 16:31:37 +0800

    注册页面第一次修改

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="register">
+    <div class="header">
+      <img src="@/assets/image/login/logo.png" />
+      <h1 @click="$router.push('/login')">织慧科技订单管理系统</h1>
+    </div>
     <div class="registerCtn">
-      <div class="enterpriseInfo">
-        <img src="@/assets/image/login/logo.png" />
-        <span>织慧科技ERP管理系统</span>
+      <div class="title">注册员工账号</div>
+      <div class="inputCtn">
+        <span class="label">登录手机号：</span>
+        <input type="text" placeholder="请输入登录的手机号" />
       </div>
-      <div class="main">
-        <div class="title">注册员工账号</div>
-        <div class="inputCtn">
-          <span class="label">登录手机号：</span>
-          <input type="text" placeholder="请输入登录的手机号" />
-        </div>
-        <div class="inputCtn">
-          <span class="label">验证码：</span>
-          <input type="text" placeholder="请输入验证码" style="width:80px"/>
-          <div class="send" @click="sendYzm">{{time}}</div>
-        </div>
-        <div class="inputCtn">
-          <span class="label">登录密码：</span>
-          <input type="text" placeholder="请输入密码" />
-        </div>
-        <div class="inputCtn">
-          <span class="label">重复密码：</span>
-          <input type="text" placeholder="请再次输入密码" />
-        </div>
-        <div class="regBtn">注册</div>
-        <div class="goBack" @click="$router.push('/login')">返回登录页</div>
+      <div class="inputCtn">
+        <span class="label">验证码：</span>
+        <input type="text" placeholder="请输入验证码" style="width:80px"/>
+        <div class="send" @click="sendYzm">{{time}}</div>
       </div>
+      <div class="inputCtn">
+        <span class="label">登录密码：</span>
+        <input type="text" placeholder="请输入密码" />
+      </div>
+      <div class="inputCtn">
+        <span class="label">重复密码：</span>
+        <input type="text" placeholder="请再次输入密码" />
+      </div>
+      <div class="regBtn">注册</div>
+      <div class="goBack" @click="$router.push('/login')">返回登录页</div>
     </div>
   </div>
 </template>
