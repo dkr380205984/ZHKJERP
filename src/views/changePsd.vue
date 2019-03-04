0988c1de69c757f97803878e7b0898d892d64ca9
Your Name <you@example.com>
2019-03-04 11:19:33 +0800

    修改秘密页面初步完成

--- src/views/changePsd.vue
+++ src/views/changePsd.vue
@@ -1,38 +1,55 @@
 <template>
   <div id="changePsd">
+    <div class="header">
+      <img src="@/assets/image/login/logo.png" />
+      <h1 @click="$router.push('/login')">织慧科技订单管理系统</h1>
+    </div>
     <div class="changePsdCtn">
-      <div class="enterpriseInfo">
-        <img src="@/assets/image/login/logo.png" />
-        <span>织慧科技ERP管理系统</span>
+      <div class="title">找回密码</div>
+      <div class="stepCtn">
+        <el-steps :active="active" finish-status="success" :align-center="true">
+          <el-step title="验证手机号"></el-step>
+          <el-step title="修改密码"></el-step>
+          <el-step title="修改成功"></el-step>
+        </el-steps>
       </div>
-      <div class="main">
-        <div class="stepCtn" :class="step?'first':'second'">
-          <div class="firstStep step">
+      <div class="stepsMain">
+        <div class="stepMainCtn" :class="{'atStep1':active===0,'atStep2':active===1,'atStep3':active===3}">
+          <div class="stepOnce step1">
             <div class="inputCtn">
-              <span class="label">手机号：</span>
-              <input type="text" placeholder="请输入手机号" />
+              <span class="label">登录手机号：</span>
+              <input type="text" placeholder="请输入登录的手机号" />
             </div>
             <div class="inputCtn">
               <span class="label">验证码：</span>
               <input type="text" placeholder="请输入验证码" style="width:80px"/>
               <div class="send" @click="sendYzm">{{time}}</div>
             </div>
-            <div class="button" @click="step=!step">下一步</div>
+            <div class="btnCtn">
+              <div class="btn" @click="active++">下一步</div>
+            </div>
           </div>
-          <div class="secondStep step">
-            <div class="goBack" @click="step=!step">返回上一页</div>
+          <div class="stepOnce step2">
             <div class="inputCtn">
-              <span class="label">输入新密码：</span>
+              <span class="label">新密码：</span>
               <input type="text" placeholder="请输入新密码" />
             </div>
             <div class="inputCtn">
               <span class="label">重复密码：</span>
-              <input type="text" placeholder="请再次输入密码" />
+              <input type="text" placeholder="请再次输入密码"/>
             </div>
-            <div class="button">完成</div>
+            <div class="btnCtn">
+              <div class="btn" @click="active--">上一步</div>
+              <div class="btn" @click="changePsd">确认修改</div>
+            </div>
+          </div>
+          <div class="stepOnce step3">
+            <div class="line1">您的密码已经修改成功!</div>
+            <div class="line2">3秒后自动跳转到登录页面<span class="dot">...</span></div>
           </div>
         </div>
       </div>
+      <div class="goBack" @click="$router.push('/login')">返回登录页</div>
     </div>
   </div>
 </template>
@@ -44,7 +61,7 @@ export default {
       time: '发送验证码',
       timer: '',
       yzmNum: 60,
-      step: true
+      active: 0
     }
   },
   methods: {
@@ -61,6 +78,10 @@ export default {
           this.timer = ''
         }
       }, 1000)
+    },
+    changePsd () {
+      this.active = this.active + 2
+      console.log(this.active)
     }
   }
 }
@@ -68,4 +89,18 @@ export default {
 
 <style lang="less" scoped>
   @import '~@/assets/css/changePsd.less';
+</style>
+<style lang="less">
+  #changePsd{
+    .el-step__head.is-success{
+      color: #10AEF5;
+      border-color: #10AEF5;
+    }
+    .el-step__title.is-success{
+      color: #10AEF5;
+    }
+    .el-step__icon.is-text{
+      border:1px solid;
+    }
+  }
 </style>
