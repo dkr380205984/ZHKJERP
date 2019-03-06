41482b88c9daca66df48a4c68ba77b8ce5e494b6
Your Name <you@example.com>
2019-03-06 09:43:09 +0800

    新增部分接口

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,11 +1,11 @@
-// import http from './http.js'
-// const baseUrl = '/api'
-// // 登录
-// const login = (params) => http.post(`${baseUrl}/auth/login`, params, 'application/json')
-// export { login }
-
-import axios from 'axios'
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = '/api'
-const login = (params) => axios.post(`/auth/login`, params)
-export { login }
+import http from './http.js'
+const baseUrl = '/api'
+// 登录
+const login = (params) => http.post(`${baseUrl}/auth/login`, params, 'application/json')
+// 注册
+const register = (params) => http.post(`${baseUrl}/auth/register`, params, 'application/json')
+// 发送验证码
+const sendCode = (params) => http.post(`${baseUrl}/auth/send_code`, params, 'application/json')
+// 获取图片上传验证
+const getToken = () => http.get(`${baseUrl}/upload/token`)
+export { login, register, sendCode, getToken }
