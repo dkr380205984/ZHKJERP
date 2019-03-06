df365ce5d19e0c0e4df4c300458fcea651767659
Your Name <you@example.com>
2019-03-06 09:42:26 +0800

    添加头部组件

---
+++ src/components/header/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="headerCompt">
+    <div class="cmpCtn">
+      <img class="cmpLogo" src="@/assets/image/index/copDefault.png" />
+      <span class="cmpName">企业名称</span>
+    </div>
+    <div class="menuCtn">
+      <div class="menuLeft">
+        <!-- <i class="el-icon-back"></i> -->
+        <span class="goBack">缺一个返回首页的按钮</span>
+        <!-- <input class="search" placeholder="搜索..." /> -->
+      </div>
+      <div class="menuRight">
+        <el-badge :value="200" :max="99" class="item">
+          <i class="el-icon-bell"></i>
+        </el-badge>
+        <i class="el-icon-setting"></i>
+        <div class="selectCtn">
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              个人中心<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>人员管理</el-dropdown-item>
+              <el-dropdown-item divided>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import './header.less'
+export default {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import mycomponent from './components'
 // 粒子特效 文档地址：https://www.jianshu.com/p/53199b842d25
 import VueParticles from 'vue-particles'
 Vue.use(VueParticles)
 Vue.use(ElementUI)
+Vue.use(mycomponent)
 
 Vue.config.productionTip = false
 
