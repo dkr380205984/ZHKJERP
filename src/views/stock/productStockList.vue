c00e788afcd707a8b782b80c6322585fa26dc299
Your Name <you@example.com>
2019-04-15 10:07:31 +0800

    库存列表页面初始化

---
+++ src/views/stock/productStockList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="productStockList">
+    <div class="head">
+      <h2>产品库存列表</h2>
+      <el-input placeholder="输入产品编号精确搜索" suffix-icon="el-icon-search" v-model="searchVal"></el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchVal: ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  @import '~@/assets/css/productStockList.less';
+</style>
